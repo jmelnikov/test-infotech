ed103ddf30fd2a17a2ff7221f3eb0a934513396e
Yuriy Melnikov <melnikov.j@gmail.com>
2025-02-19 14:19:04 +0300

    Update params.php

--- app/config/params.php
+++ app/config/params.php
@@ -1,8 +1,9 @@
 <?php
-
+// Для smsPilotApiKey используется ключ эмулятора.
+// Если ключ перестанет работать, то новый можно взять здесь: https://smspilot.ru/apikey.php
 return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-    'smsPilotApiKey' => 'XXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZXXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZ', // Ключ эмулятора
+    'smsPilotApiKey' => 'XXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZXXXXXXXXXXXXYYYYYYYYYYYYZZZZZZZZ',
 ];
